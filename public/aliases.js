66196c352f14b5d11b3c80ec68db745266ef0ddf
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-03-21 22:34:32 +0530

    Initial commit

--- public/aliases.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const submit = (user) => {
-    console.log(`${user.name} hello!`);
-};
--- src/aliases.ts
+++
@@ -1,6 +0,0 @@
-type StringOrNum = string | number;
-type ObjectWithName = {name: string, uid: StringOrNum};
-
-const submit = (user: ObjectWithName) => {
-    console.log(`${user.name} hello!`);
-}
