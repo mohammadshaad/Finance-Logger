ef37ee07f05b85477da43792be5f349f7cf03a16
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-03-21 22:12:41 +0530

    Function Signatures

--- public/functionSignature.js
+++ public/functionSignature.js
@@ -1,5 +1,18 @@
 "use strict";
-let greet1 = (a, b) => void ;
+let greet1;
 greet = (name, greetings) => {
     console.log(`${name}`);
+};
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+let logDetails;
+logDetails = (ninjaStudent) => {
+    console.log(`${ninjaStudent.name} is super hero with mobile number ${ninjaStudent.phone}`);
 };
